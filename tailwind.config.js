fd3194c6f2e165def0bbf5b576e899c5e41c846a
moAminee <amineboukhalit@gmail.com>
2023-12-04 17:14:11 +0100

    UI Ready For Desktop

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+
+
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors: {
+      
+      'lowpurple' : '#C790FE',
+      'midpurple' : '#C085FC',
+      'highpurple' : '#7F00FF',
+      'black':'#000000',
+      'white':'#FFFFFF',
+      'lightred' : '#8E008C',
+      'nightred' : '#7744AA',
+
+
+
+    }
+    
+  },
+  
+  plugins: [],
+  
+}
+
